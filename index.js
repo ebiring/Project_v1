f0a76b0753245424a76b4793488c8a73bb1da37f
ebiring <153887309+ebiring@users.noreply.github.com>
2023-12-15 15:05:16 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+require("dotenv").config();
+
+const { Client, GatewayIntentBits } = require("discord.js");
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds, 
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent // If you need to access message content
+  ]
+});
+
+client.once("ready", () => {
+  console.log("Bot is online!");
+});
+
+client.on("messageCreate", (message) => {
+  if (message.content === "!ping") {
+    message.channel.send("Pong!");
+  }
+});
+
+client.login(process.env.DISCORD_TOKEN);
